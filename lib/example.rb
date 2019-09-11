c8ebe0a7f69f510808654a732880b376a3baf434
Joshua Rodriguez <josh030596@gmail.com>
2019-09-11 02:41:29 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,16 @@
-# Use this space to code along with the readme
+def say_hello_ten_times
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase 
+  puts phrase 
+  puts phrase
+  puts phrase
+  puts phrase 
+  puts phrase 
+  puts phrase 
+end 
 
 phrase = "Hello World!"
 puts phrase
